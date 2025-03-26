58f765bd27ce295eb3b45aad2e016d38630cbe3b
SakuraNiroku <liangyin17@outlook.com>
2025-03-26 21:53:42 +0800

    [-] remove attack ab [+] add autoblock like vape's ab

--- src/main/java/today/opai/example/modules/AutoBlock.java
+++ src/main/java/today/opai/example/modules/AutoBlock.java
@@ -6,18 +6,17 @@ import today.opai.api.events.EventPacketSend;
 import today.opai.api.interfaces.game.entity.LocalPlayer;
 import today.opai.api.interfaces.game.network.NetPacket;
 import today.opai.api.interfaces.game.network.client.CPacket02UseEntity;
+import today.opai.api.interfaces.game.network.client.CPacket0ASwing;
 import today.opai.api.interfaces.modules.values.BooleanValue;
 import today.opai.example.EventScriptModule;
 import today.opai.example.ExampleExtension;
 
 public class AutoBlock extends EventScriptModule {
 
-    public final BooleanValue onlySword = ExampleExtension.getAPI().getValueManager().createBoolean("Only Sword", true);
 
     public AutoBlock() {
         super("AutoBlock","send attack packet and use right click",EnumModuleCategory.COMBAT);
         super.setEventHandler(this);
-        super.addValues(onlySword);
     }
 
     @Override
@@ -25,14 +24,9 @@ public class AutoBlock extends EventScriptModule {
         LocalPlayer player = ExampleExtension.openAPI.getLocalPlayer();
 
         NetPacket packet = event.getPacket();
-        if (packet instanceof CPacket02UseEntity) {
-            CPacket02UseEntity c02 = (CPacket02UseEntity) packet;
-            if (c02.getAction() == EnumUseEntityAction.ATTACK) {
-                if (onlySword.getValue()){
-                    if (!player.getInventory().getMainInventory().get(player.getItemSlot()).getName().contains("sword")){
-                        return;
-                    }
-                }
+
+        if (packet instanceof CPacket0ASwing) {
+            if (player.getInventory().getMainInventory().get(player.getItemSlot()).getName().contains("sword")){
                 ExampleExtension.openAPI.getLocalPlayer().rightClickMouse();
             }
         }
